7e0e961a3eebbd23ed5c472d6d7078ce74f3bf9b
Exerosis <exerosis@gmail.com>
2023-01-09 02:19:18 -0500

    ~ Debugging stuck on write.

--- networking.go
+++ networking.go
@@ -37,7 +37,7 @@ func (tcp *TcpMulticaster) send(buffer []byte) error {
 					//fmt.Println("Wrote for ", connection.RemoteAddr().String())
 					break
 				}
-				fmt.Println("Timed out! ", connection.RemoteAddr().String())
+				fmt.Printf("Timed out! %s %s", reason, connection.RemoteAddr().String())
 			}
 
 			//if reason != nil {
