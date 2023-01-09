59088bf3271fa19c6732647edc7d6d9bc032d107
Exerosis <exerosis@gmail.com>
2023-01-09 05:34:52 -0500

    ~ Testing scaling

--- node.go
+++ node.go
@@ -63,7 +63,7 @@ func Node(
 					amount = atomic.LoadUint32(&count)
 				}
 				if amount >= AVERAGE {
-					percent, reason := cpu.Percent(0, false)
+					percent, reason := cpu.Percent(0, true)
 					if reason != nil {
 						return reason
 					}
