ad6677b55db26d4aa20fe3c3a0c356885db1e8fe
Exerosis <exerosis@gmail.com>
2023-01-10 05:46:03 -0500

    ~ Testing dummy data.

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-const COUNT = 10_000_000
+const COUNT = 1_000_000
 const AVERAGE = 10_000
 const INFO = false
 
@@ -24,7 +24,7 @@ const INFO = false
 // 256 - 139 (1m)
 // 512 - 138 (1m)
 // 1024 - 138 (1m)
-const Pipes = 2048
+const Pipes = 1024
 
 const Multiplier = 2000
 const SizeBuffer = 10 * Multiplier
