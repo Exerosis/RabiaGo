ace962d3ce4abfe220e9698695fdffb66161c2d9
Exerosis <exerosis@gmail.com>
2023-01-10 05:21:03 -0500

    ~ Testing dummy data.

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ const INFO = false
 // 1024 - 138 (1m)
 const Pipes = 256
 
-const Multiplier = 500
+const Multiplier = 1000
 const SizeBuffer = 10 * Multiplier
 const SizeProvider = 10 * Multiplier
 const SizeVote = 3 * Multiplier
