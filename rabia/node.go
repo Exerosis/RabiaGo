efcef2e2ac1085af3fdd3c11f6bb54af2ab9b86f
Home <exerosis@gmail.com>
2024-01-04 15:59:39 -0500

    ~ Test lower delay.

--- rabia/node.go
+++ rabia/node.go
@@ -299,7 +299,7 @@ func (node *node) Run() error {
 				//	}
 				//}
 				//three++
-				time.Sleep(5 * time.Microsecond)
+				time.Sleep(20 * time.Microsecond)
 				var next = queue.Take()
 				//if next == nil {
 				//	println("considering noop ", queue.Size())
