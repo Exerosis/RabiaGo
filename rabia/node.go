81e72d60b60b1f835daf973983df835299cf6b51
Exerosis <exerosis@gmail.com>
2024-01-12 13:03:44 -0500

    ~ Test

--- rabia/node.go
+++ rabia/node.go
@@ -392,7 +392,12 @@ func (node *node) Consume(block func(uint64, uint64, []byte) error) error {
 		if proposal == SKIP {
 			continue
 		}
-		var data = node.messages.WaitFor(proposal)
+		node.proposeLock.RLock()
+		data, present := node.messages.Get(proposal)
+		node.proposeLock.RUnlock()
+		if !present {
+			data = make([]byte, 0)
+		}
 		reason := block(i, proposal, data)
 		if reason != nil {
 			return reason
