4445f7bf86e272fbb651e45741310c31bb198d33
money <lucasphone75@gmail.com>
2024-01-21 23:09:33 -0700

    rabia debugs

--- rabia/smr.go
+++ rabia/smr.go
@@ -102,7 +102,7 @@ func (log Log) SMR(
 		} else {
 			state = 0
 		}
-		if false { // highest == 1 || highest >= log.N-log.F
+		if highest == 1 || highest >= log.N-log.F { // false
 			info("Into optimization instead.\n")
 			if highest == 1 {
 				proposed = SKIP
