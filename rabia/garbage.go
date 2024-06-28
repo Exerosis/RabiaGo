9f1a83d97c0e75d7f12f9a1ad3061e12d279cde0
Home <exerosis@gmail.com>
2024-06-28 05:01:17 -0400

    ~ Fix types.

--- rabia/garbage.go
+++ rabia/garbage.go
@@ -25,28 +25,28 @@ func (comparator *Comparator) Compare(o1, o2 any) int {
 	return comparator.Comparison(o1, o2)
 }
 
-func ComparingUint64(o1, o2 any) int {
-	if o1.(uint64) > o2.(uint64) {
+func ComparingUint64(a, b uint64) int {
+	if a > b {
 		return -1
-	} else if o1.(uint64) == o2.(uint64) {
+	} else if a == b {
 		return 0
 	}
 	return 1
 }
 
-func ComparingProposals(o1, o2 any) int {
+func ComparingProposals(a, b uint64) int {
 	// First, compare the timestamps (lower 32 bits)
 	var first = ComparingUint64(
-		o1.(Identifier).Value&0xFFFFFFFF,
-		o2.(Identifier).Value&0xFFFFFFFF,
+		a&0xFFFFFFFF,
+		b&0xFFFFFFFF,
 	)
 	if first != 0 {
 		return first
 	}
 	// If timestamps are equal, compare the random parts (higher 32 bits)
 	return ComparingUint64(
-		o1.(Identifier).Value>>32,
-		o2.(Identifier).Value>>32,
+		a>>32,
+		b>>32,
 	)
 }
 
