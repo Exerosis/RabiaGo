f292e6113b25dab9f45519ea93ae6c25f811e7bd
Home <exerosis@gmail.com>
2024-10-07 09:04:20 +0900

    Revert "~ Fix types."
    This reverts commit 9f1a83d97c0e75d7f12f9a1ad3061e12d279cde0.

--- rabia/garbage.go
+++ rabia/garbage.go
@@ -25,28 +25,28 @@ func (comparator *Comparator) Compare(o1, o2 any) int {
 	return comparator.Comparison(o1, o2)
 }
 
-func ComparingUint64(a, b uint64) int {
-	if a > b {
+func ComparingUint64(o1, o2 any) int {
+	if o1.(uint64) > o2.(uint64) {
 		return -1
-	} else if a == b {
+	} else if o1.(uint64) == o2.(uint64) {
 		return 0
 	}
 	return 1
 }
 
-func ComparingProposals(a, b uint64) int {
+func ComparingProposals(o1, o2 any) int {
 	// First, compare the timestamps (lower 32 bits)
 	var first = ComparingUint64(
-		a&0xFFFFFFFF,
-		b&0xFFFFFFFF,
+		o1.(Identifier).Value&0xFFFFFFFF,
+		o2.(Identifier).Value&0xFFFFFFFF,
 	)
 	if first != 0 {
 		return first
 	}
 	// If timestamps are equal, compare the random parts (higher 32 bits)
 	return ComparingUint64(
-		a>>32,
-		b>>32,
+		o1.(Identifier).Value>>32,
+		o2.(Identifier).Value>>32,
 	)
 }
 
